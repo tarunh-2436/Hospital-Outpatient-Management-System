08f4d4f43dc29abdf661d5634e280f955c3493ac
tarunh-2436 <152145012+tarunh-2436@users.noreply.github.com>
2024-06-09 18:35:43 +0530

    Organized the files into folders and uplaoding the final files

---
+++ Hospital Out Patient Management System/Doctor_App.py
@@ -0,0 +1,117 @@
+from flask import Flask, render_template, request, redirect, url_for
+import random, pickle, datetime, os
+from functions.classes import TreeNode,Hospital,Patient,Doctor
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('doctor_home.html')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'GET':
+        return render_template("doctor_login.html")
+    
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        Doctors = Hospital.Retrieve_Doctors()[0]
+        for doctor in Doctors:
+            if doctor.Username == username and doctor.Password == password:
+                return redirect(url_for('choosepatient',doctor=doctor.Name))
+            
+        return render_template('doctor_login.html')
+
+@app.route('/choosepatient/<doctor>',methods=['GET','POST'])
+def choosepatient(doctor) :
+    Doctors=Hospital.Retrieve_Doctors()[0]
+    for Doctor in Doctors :
+        if Doctor.Name == doctor :
+            Doctor_Obj = Doctor
+    date=datetime.date.today().isoformat()
+    try :
+        Appointments = str(Doctor_Obj.Appointments[date])[1:-1]
+    except KeyError :
+        return redirect(url_for('noappointments'))
+    else :
+        if request.method == 'GET' :
+            return render_template('paid.html',doctor=doctor,appointments = Appointments)
+        if request.method == 'POST' :
+            patient_id = int(request.form['Patient ID'])
+            Patients = Hospital.Retrieve_Patients()
+            for Patient in Patients :
+                if Patient.Patient_ID == patient_id :
+                    Patient_Obj=Patient
+                    return redirect(url_for('patientvisit',doctor=doctor,patient=patient_id))
+            return render_template('paid.html',doctor=doctor,appointments = Appointments) 
+            
+        
+@app.route('/patientvisit/<doctor>/<patient>',methods=['GET','POST'])
+def patientvisit(doctor,patient) :
+    Doctors=Hospital.Retrieve_Doctors()[0]
+    for Doctor in Doctors :
+        if Doctor.Name == doctor :
+            Doctor_Obj = Doctor
+            break
+    Patients = Hospital.Retrieve_Patients()
+    for Patient in Patients :
+        if Patient.Patient_ID == int(patient) :
+            Patient_Obj=Patient
+            break
+    if request.method == 'GET' :
+        Name = Patient_Obj.Name
+        Age = Patient_Obj.Age
+        Gender = Patient_Obj.Gender
+        Hypertension = Patient_Obj.Medical_Details['Hypertension']+' ('+Patient_Obj.Medical_Details['Hypertension Duration']+')'
+        Diabetes = Patient_Obj.Medical_Details['Diabetes']+' ('+Patient_Obj.Medical_Details['Diabetes Duration']+')'
+        Heart_Disease = Patient_Obj.Medical_Details['Heart Disease']+' ('+Patient_Obj.Medical_Details['Heart Disease Duration']+')'
+        Asthma = Patient_Obj.Medical_Details['Asthma']+' ('+Patient_Obj.Medical_Details['Asthma Duration']+')'
+        Epilepsy = Patient_Obj.Medical_Details['Epilepsy']+' ('+Patient_Obj.Medical_Details['Epilepsy Duration']+')'
+        Stroke = Patient_Obj.Medical_Details['Stroke']+' ('+Patient_Obj.Medical_Details['Stroke Duration']+')'
+        Peptic_Ulcer = Patient_Obj.Medical_Details['Peptic Ulcer']+' ('+Patient_Obj.Medical_Details['Peptic Ulcer Duration']+')'
+        Tuberculosis = Patient_Obj.Medical_Details['Tuberculosis']+' ('+Patient_Obj.Medical_Details['Tuberculosis Duration']+')'
+        Surgery = Patient_Obj.Medical_Details['Surgery']+' ('+Patient_Obj.Medical_Details['Surgery Details']+')'
+        Allergy = Patient_Obj.Medical_Details['Allergy']+' ('+Patient_Obj.Medical_Details['Allergy Details']+')'
+        Smoking = Patient_Obj.Medical_Details['Smoking']
+        Drinking = Patient_Obj.Medical_Details['Drinking']
+        if Patient_Obj.Patient_Stack != [] :
+            Last_Visit = Patient_Obj.Patient_Stack[-1]
+            Last_Visit_Date = Last_Visit['Date']
+            Last_Visit_Notes = Last_Visit["Doctor's Notes"]
+            Last_Visit_Prescription = Last_Visit['Prescription']
+            return render_template('patient_visit.html',doctor=doctor,patient=patient,Name=Name,Age=Age,Gender=Gender,Hypertension=Hypertension,Diabetes=Diabetes,Heart_Disease=Heart_Disease,Asthma=Asthma,Epilepsy=Epilepsy,Stroke=Stroke,Peptic_Ulcer=Peptic_Ulcer,Tuberculosis=Tuberculosis,Surgery=Surgery,Allergy=Allergy,Smoking=Smoking,Drinking=Drinking,Last_Visit_Date=Last_Visit_Date,Last_Visit_Notes=Last_Visit_Notes,Last_Visit_Prescription=Last_Visit_Prescription)
+        else :
+            return render_template('patient_visit.html',doctor=doctor,patient=patient,Name=Name,Age=Age,Gender=Gender,Hypertension=Hypertension,Diabetes=Diabetes,Heart_Disease=Heart_Disease,Asthma=Asthma,Epilepsy=Epilepsy,Stroke=Stroke,Peptic_Ulcer=Peptic_Ulcer,Tuberculosis=Tuberculosis,Surgery=Surgery,Allergy=Allergy,Smoking=Smoking,Drinking=Drinking,Last_Visit_Notes='',Last_Visit_Prescription='')
+    if request.method == 'POST' :
+        Visit_Notes=request.form['notes']
+        Visit_Prescription=request.form['prescription']
+        Visit_Details={"Date":datetime.date.today().isoformat(), "Doctor's Notes":Visit_Notes, 'Prescription':Visit_Prescription}
+        Patient_Obj.Patient_Stack.append(Visit_Details)
+        f = open('Patient Details.pkl','rb')
+        g = open('Dummy.pkl','wb')
+        while True :
+            try :
+                Patient = pickle.load(f)
+            except EOFError :
+                break
+            else :
+                if Patient.Patient_ID == Patient_Obj.Patient_ID :
+                    pickle.dump(Patient_Obj,g)
+                else :
+                    pickle.dump(Patient,g)
+        f.close()
+        g.close()
+        os.remove('Patient Details.pkl')
+        os.rename('Dummy.pkl','Patient Details.pkl')
+        return redirect(url_for('choosepatient',doctor=doctor))
+    
+@app.route('/noappointments', methods=['GET','POST'])
+def noappointments() :
+    if request.method == 'GET' :
+        return render_template('no_app.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
---
+++ Hospital Out Patient Management System/Patient_App.py
@@ -0,0 +1,177 @@
+from flask import Flask, render_template, request, redirect, url_for
+import random, pickle, datetime
+from functions.classes import TreeNode,Hospital,Patient,Doctor
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('patient_home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        Patients = Hospital.Retrieve_Patients()
+        for Patient_Obj in Patients :
+            if Patient_Obj.Username == username and Patient_Obj.Password == password :
+                return redirect(url_for('doctors',patient=Patient_Obj.Name))
+        return render_template('patient_log.html')
+    return render_template('patient_log.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        New_Patient = Patient()
+        New_Patient.Name = request.form['Name']
+        New_Patient.Age = request.form['Age']
+        New_Patient.Gender = request.form['Gender']
+        New_Patient.Phone_No = request.form['Phone No']
+        New_Patient.Address = request.form['Address']
+        New_Patient.Username = request.form['Username']
+        New_Patient.Password = request.form['Password']
+        medical_history = request.form.getlist('Medical History')   
+        Medical_Details = {}         
+        if 'Hypertension'.upper() in medical_history :
+            Medical_Details['Hypertension'] = 'YES'
+            Medical_Details['Hypertension Duration'] = request.form['Hypertension Duration']
+        else :
+            Medical_Details['Hypertension'] = 'NO'
+            Medical_Details["Hypertension Duration"] = 'NA'
+        if 'Diabetes'.upper() in medical_history :
+            Medical_Details['Diabetes'] = 'YES'
+            Medical_Details['Diabetes Duration'] = request.form['Diabetes Duration']
+        else :
+            Medical_Details['Diabetes'] = 'NO'
+            Medical_Details["Diabetes Duration"] = 'NA'
+        if 'Heart Disease'.upper() in medical_history :
+            Medical_Details['Heart Disease'] = 'YES'
+            Medical_Details['Heart Disease Duration'] = request.form['Heart Disease Duration']
+        else :
+            Medical_Details['Heart Disease'] = 'NO'
+            Medical_Details["Heart Disease Duration"] = 'NA'
+        if 'Asthma'.upper() in medical_history :
+            Medical_Details['Asthma'] = 'YES'
+            Medical_Details['Asthma Duration'] = request.form['Asthma Duration']
+        else :
+            Medical_Details['Asthma'] = 'NO'
+            Medical_Details["Asthma Duration"] = 'NA'
+        if 'Epilepsy'.upper() in medical_history :
+            Medical_Details['Epilepsy'] = 'YES'
+            Medical_Details['Epilepsy Duration'] = request.form['Epilepsy Duration']
+        else :
+            Medical_Details['Epilepsy'] = 'NO'
+            Medical_Details["Epilepsy Duration"] = 'NA'
+        if 'Stroke'.upper() in medical_history :
+            Medical_Details['Stroke'] = 'YES'
+            Medical_Details['Stroke Duration'] = request.form['Stroke Duration']
+        else :
+            Medical_Details['Stroke'] = 'NO'
+            Medical_Details["Stroke Duration"] = 'NA'
+        if 'Peptic Ulcer'.upper() in medical_history :
+            Medical_Details['Peptic Ulcer'] = 'YES'
+            Medical_Details['Peptic Ulcer Duration'] = request.form['Peptic Ulcer Duration']
+        else :
+            Medical_Details['Peptic Ulcer'] = 'NO'
+            Medical_Details["Peptic Ulcer Duration"] = 'NA'
+        if 'Tuberculosis'.upper() in medical_history :
+            Medical_Details['Tuberculosis'] = 'YES'
+            Medical_Details['Tuberculosis Duration'] = request.form['Tuberculosis Duration']
+        else :
+            Medical_Details['Tuberculosis'] = 'NO'
+            Medical_Details["Tuberculosis Duration"] = 'NA'
+        if 'Surgical'.upper() in medical_history :
+            Medical_Details['Surgery'] = 'YES'
+            Medical_Details['Surgery Detailsn'] = request.form['Surgery Details']
+        else :
+            Medical_Details['Surgery'] = 'NO'
+            Medical_Details["Surgery Details"] = 'NA'
+        if 'Allergy'.upper() in medical_history :
+            Medical_Details['Allergy'] = 'YES'
+            Medical_Details['Allergy Details'] = request.form['Allergy Details']
+        else :
+            Medical_Details['Allergy'] = 'NO'
+            Medical_Details["Allergy Details"] = 'NA'
+        if 'Smoking'.upper() in medical_history :
+            Medical_Details['Smoking'] = 'YES'
+        else :
+            Medical_Details['Smoking']='NO'
+        if 'Drinking'.upper() in medical_history :
+            Medical_Details['Drinking'] = 'YES'
+        else :
+            Medical_Details['Drinking'] = 'NO'
+        New_Patient.Medical_Details = Medical_Details
+        New_Patient.Patient_ID = New_Patient.generate_patient_ID()
+
+        f = open('Patient Details.pkl','ab')
+        pickle.dump(New_Patient,f)
+        f.close()
+
+        return redirect(url_for('doctors',patient=New_Patient.Name))
+    return render_template('regis.html')
+
+@app.route('/doctors/<patient>', methods=['GET','POST'])
+def doctors(patient):
+    if request.method=='POST' :
+        required_doctor=request.form["Doctor"]
+        doctors = Hospital.Retrieve_Doctors()[0]
+        for doctor_obj in doctors :
+            if doctor_obj.Name == required_doctor :
+                return redirect(url_for('appointment',patient=patient, doctor=doctor_obj.Name))
+        return render_template('docs(1).html',patient=patient)
+
+    return render_template('docs(1).html',patient=patient)
+
+@app.route('/appointment/<patient>/<doctor>',methods=['GET','POST'])
+def appointment(doctor,patient):
+    f = open('Hospital Details.pkl','rb')
+    BSS = pickle.load(f)
+    f.close()
+    for Department in BSS.root.children :
+            for Doctor in Department.children :
+                if Doctor.Name == doctor :
+                    Doctor_Obj = Doctor
+    for Patient in Hospital.Retrieve_Patients() :
+        if Patient.Name == patient :
+            Patient_Obj = Patient
+            break
+    slots = str(Doctor_Obj.Slots)[1:-1]
+    if request.method=='GET' :
+        return render_template('app.html',name=doctor, slots=slots, patient=patient, doctor=doctor)
+    if request.method=='POST' :
+        Required_Date = request.form['Date']
+        Required_Time = request.form['Time']
+        date=datetime.datetime(int(Required_Date[:4]),int(Required_Date[5:7]),int(Required_Date[8:]))
+        day_of_week=date.weekday()
+        '''Doctor_Obj.Department != 'General Medicine' and day_of_week == 6 or '''
+        if Required_Time not in Doctor_Obj.Slots :
+            return redirect(url_for('unavailable',patient=patient,doctor=doctor))
+        else :
+            if Required_Date not in Doctor_Obj.Appointments :
+                Doctor_Obj.Appointments[Required_Date] = {Required_Time:Patient_Obj.Patient_ID}
+                f = open('Hospital Details.pkl','wb')
+                pickle.dump(BSS,f)
+                f.close()
+                return render_template('success.html')
+            else :
+                for booked_time in Doctor_Obj.Appointments[Required_Date] :
+                    if Required_Time==booked_time :
+                        return redirect(url_for('unavailable',patient=patient,doctor=doctor))
+                Doctor_Obj.Appointments[Required_Date][Required_Time] = Patient_Obj.Patient_ID
+                f = open('Hospital Details.pkl','wb')
+                pickle.dump(BSS,f)
+                f.close()
+                return render_template('success.html')
+        
+@app.route('/unavailable/<patient>/<doctor>',methods=['GET','POST'])
+def unavailable(patient,doctor) :
+    if request.method == 'GET' :
+        return render_template('fail.html',patient=patient,doctor=doctor)
+    if request.method == 'POST' :
+        return redirect(url_for('appointment',patient=patient,doctor=doctor))
+
+
+if __name__ == "__main__" :
+
+    app.run(debug=True)
